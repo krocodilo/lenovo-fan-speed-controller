9da3f29c1393c4515c5189e4f7e0a72860c3c71c
David Cruz <70323546+krocodilo@users.noreply.github.com>
2022-05-05 22:24:46 +0100

    improve

--- FanSpeed.sh
+++ FanSpeed.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-if [ $# -ne 1 ] ; then
-	echo -e "\n\n\n    COMMAND:   FanSpeed <level>\n"
-	echo "    - Level can be between '0' (disabled) and '7', 'full-speed', or 'auto'."
-	echo -e "\n\n        You can also use 'FanSpeed show' to print the current level\n\n\n"
-	exit 1
-fi
+
+name=$0
+
+printHelp() {
+	echo -e "\n\t COMMAND:  $name <level>\n"
+	echo -e "\t - Level can be between '0' (disabled) and '7', 'full-speed', or 'auto'."
+	echo -e "\n\n\t\t You can also use '$name show' to print the current level\n\n\n"
+}
 
 echo -e "\n\n"
+
+if [ $# -ne 1 ]; then
+	printHelp
+	exit 1
+fi
 
 case $1 in
 	0|1|2|3|4|5|6|7|auto|full-speed)
@@ -16,8 +23,13 @@ case $1 in
 	show)
 		grep "level:" /proc/acpi/ibm/fan
 		;;
+	-h|-help|--help)
+		printHelp
+		exit 0
+		;;
 	*)
-		echo -e "    UNSUPORTED ARGUMENT\n\n\n"
+		echo -e "\t !! UNSUPORTED ARGUMENT !!"
+		printHelp
 		exit 2
 		;;
 esac
